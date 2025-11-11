8aa51aebd1af36eaec057a46049a73766f7777cd
Luca Britten <lucabritten17@icloud.com>
2025-11-11 20:33:06 +0100

    Integrate Strava API functionality with authentication, token management, and activity upload capabilities.

---
+++ src/main/java/com/britten/fittools/controller/StravaController.java
@@ -0,0 +1,43 @@
+package com.britten.fittools.controller;
+
+import com.britten.fittools.integration.strava.TokenResponse;
+import com.britten.fittools.service.StravaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@CrossOrigin(origins = "http://127.0.0.1:5500")
+@RestController
+@RequestMapping("/api/strava")
+public class StravaController {
+
+    @Autowired
+    private StravaService stravaService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file, @RequestHeader("Authorization") String bearerToken) {
+        try {
+            var response = stravaService.uploadActivity(file, bearerToken);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/authorize")
+    public ResponseEntity<String> getAuthorizationUri(){
+        return ResponseEntity.ok(stravaService.getAuthUrl());
+    }
+
+    @GetMapping("/callback")
+    public ResponseEntity<TokenResponse> callback(@RequestParam String code){
+        try{
+            TokenResponse tokenResponse = stravaService.exchangeToken(code);
+            return ResponseEntity.ok(tokenResponse);
+        }
+        catch (Exception e){
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+}
